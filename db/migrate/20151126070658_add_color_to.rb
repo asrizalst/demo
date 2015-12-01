ce64e9d537e2c01664406a2ead4f9cef282dba52
Asrizal <asrizal.st@gmail.com>
2015-11-30 20:55:13 -0800

    added price field products

--- db/migrate/20151126070658_add_color_to.rb
+++
@@ -1,4 +0,0 @@
-class AddColorTo < ActiveRecord::Migration
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128171821) do
+ActiveRecord::Schema.define(version: 20151201030752) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151128171821) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.integer  "price"
   end
 
 end
